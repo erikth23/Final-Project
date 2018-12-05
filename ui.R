0a782f8f856ffa8902ea1b31b05705da193e854f
Erik Thomas-Hommere <erikth2355@gmail.com>
2018-12-05 00:00:18 -0800

    Update shiny app

--- ui.R
+++ ui.R
@@ -1,38 +1,52 @@
-# Author: Raphael Manansala
-
-# Load packages required
 library(shiny)
 
-# Load Global Temeprature By Cities data 
-global_cities <- read.csv(file = "GlobalLandTemperaturesByMajorCity.csv", sep = ",", stringsAsFactors = FALSE) 
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
+my.ui <- navbarPage(
+  "Global Climate Change",
   
-  # Application title
-  titlePanel("Climate Change In Major Cities"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-      
-      # This textbox will let users input cities that they want by name
-      selectInput("city",
-                label = "City",
-                selected = "Los Angeles", # default city
-                choices = unique(global_cities$City)
-                )
-      
-    ),
-
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      # This is the UI for the scatter plot
-      plotOutput("scatterPlot"),
-      # This is the UI for the summary text
-      textOutput("summary")
-       
-    )
+  tabPanel("Home",
+           sidebarLayout(
+             sidebarPanel(),
+             mainPanel(
+               textOutput("home")
+             )
+           )),
+  tabPanel("Major Cities",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput("city",
+                           label = "City",
+                           selected = "Los Angeles", # default city
+                           choices = unique(global_cities$City)
+               )
+              ),
+             mainPanel(
+               plotOutput("major_city_plot"),
+               textOutput("city_text")
+             )
+           )
+  ),
+  tabPanel("Countries",
+           sidebarLayout(
+             sidebarPanel(
+               selectInput('country',
+                           label = 'Country',
+                           choices = unique(data$Country)
+                           )
+               ),
+             mainPanel(
+               plotOutput("country_plot")
+             )
+           )
+  ),
+  tabPanel("United States",
+           sidebarPanel(
+             sliderInput("date",
+                         "Enter Decade to Analyze",
+                         1850, 2010, 1850,
+                         step = 10)
+           ),
+           mainPanel(
+             plotOutput("state_plot")
+           )
   )
-))
+)
