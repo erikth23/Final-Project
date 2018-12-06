75bdf01f7fadc6de6c6aef410f766aac43c89d2a
Julia Summers <juliasummers@Julias-MacBook-Air.local>
2018-12-05 20:57:16 -0800

    last changes

--- Scripts/server_Julia.R
+++ Scripts/server_Julia.R
@@ -6,6 +6,9 @@ library(dplyr)
 library(ggplot2)
 
 #Preparing files:
+getwd()
+setwd("~/Desktop/INFO201/ProjectFinal201/Final-Project/Scripts")
+
 
 data <- read.csv("data/GlobalLandTemperaturesByCountry.csv", sep = ",", stringsAsFactors = FALSE)
 
@@ -23,7 +26,7 @@ temperaturebycountry_plot <- function(country_selected){
   country_data <- country_average_temperature_plot(country_selected)
   plot <- country_data%>%
     ggplot() +
-    geom_bar(aes(x = year, y = AverageTemperature), stat = 'identity') + 
+    geom_bar(aes(x = year, y = AverageTemperature), fill = "blue", stat = 'identity') + 
     ggtitle (paste ('Average Temperatures in',country_selected))
   return(plot)
 }
@@ -39,10 +42,12 @@ shinyServer(function(input, output){
 
 generateCountryText <- function(){
   paste("This graph is showing average temperatures of each decade in countries around the world in the period of 1850-2010. 
-        Based on the plotted data, it is clear that begining with Industrial revolution temperatures in all countries have become higher, 
-        with considerable increase in the past decade in only certain countries. This could also point to other atributes that contribute to such 
-        a difference in temperatures and questions as to why some countries experience global warming more than others.
-        The bar graph of each country is unique, and its data can tell a story beyong climate change.")
+         Based on the plotted data, it is clear that begining with Industrial revolution temperatures in all countries have started rising, 
+        with considerable increase in the past decade in certain countries. For example, in Canada, the average temperatures have risen 
+        2.5 degrees since 1850, where 1 degree increase happened in the last decade alone. Such trend is seen in many countries and is worth exploring further.
+        However, Japan, for instance had lesser of an increase in average temperatures in the past decade than many other developed nations.
+        Same pertains to South Korea.It appears also that some countries experience global warming more than others. Additionally, the bar graph of each country 
+        is unique, and its data can tell a story beyong climate change.")
   
 }
 
